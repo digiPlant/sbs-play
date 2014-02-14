cde4c6d6ff89a7078adf60ba3705ab54dd61abe5
Peter Hagström <peter@digiplant.se>
2014-02-14 16:07:03 +0100

    git-sbs

--- framework/pym/play/commands/base.py
+++ framework/pym/play/commands/base.py
@@ -12,12 +12,13 @@ import signal
 
 from play.utils import *
 
-COMMANDS = ['run', 'new', 'new-sbs', 'clean', 'test', 'autotest', 'auto-test', 'id', 'new,run', 'clean,run', 'modules']
+COMMANDS = ['run', 'new', 'new-sbs', 'git-sbs', 'clean', 'test', 'autotest', 'auto-test', 'id', 'new,run', 'clean,run', 'modules']
 
 HELP = {
     'id': "Define the framework ID",
     'new': "Create a new application",
     'new-sbs': "Create a new SBS Manager",
+    'git-sbs': "Setup git environment for SBS Manager",
     'clean': "Delete temporary files (including the bytecode cache)",
     'run': "Run the application in the current shell",
     'test': "Run the application in test mode in the current shell",
@@ -38,6 +39,8 @@ def execute(**kargs):
         new(app, args, env, cmdloader)
     if command == 'new-sbs' or command == 'new-sbs,run':
         newSBS(app, args, env, cmdloader)
+    if command == 'git-sbs':
+        gitSBS(app, args, env, cmdloader)
     if command == 'clean' or command == 'clean,run':
         clean(app)
     if command == 'new,run' or command == 'clean,run' or command == 'run':
@@ -168,18 +171,22 @@ def newSBS(app, args, env, cmdloader=None):
     if application_name == "":
         application_name = os.path.basename(app.path)
 
-    default_email = raw_input("~ Default email address (staff@digiplant.se if empty)? [%s] " % os.path.basename(app.path))
+    default_email = raw_input("~ Default email address? [staff@digiplant.se] ")
     if default_email == "":
         default_email = "staff@digiplant.se"
-    srv_user = raw_input("~ Server user name? [%s] " % os.path.basename(app.path))
-    db_name = raw_input("~ Name of the database? [%s] " % os.path.basename(app.path))
-    server_host = raw_input("~ Domain name of the deployment server (ansok.server.se if empty)? [%s] " % os.path.basename(app.path))
+    srv_user = raw_input("~ Server user name? [serverUser] ")
+    if srv_user == "":
+        srv_user = "serverUser"
+    db_name = raw_input("~ Name of the database? [sbsmgr_x] ")
+    if srv_user == "":
+        srv_user = "sbsmgr_x"
+    server_host = raw_input("~ Domain name of the deployment server? [ansok.server.se] ")
     if server_host == "":
         server_host = "ansok.server.se"
-    git_repo = raw_input("~ Name of the git repository (used in deployment, git@github.com:digiPlant/dummy.git if empty)? [%s] " % os.path.basename(app.path))
+    git_repo = raw_input("~ Name of the git repository (used in deployment)? [git@github.com:digiPlant/dummy.git] ")
     if git_repo == "":
         git_repo = "git@github.com:digiPlant/dummy.git"
-    service_name = raw_input("~ Name of the service on the deployment server (play if empty)? [%s] " % os.path.basename(app.path))
+    service_name = raw_input("~ Name of the service on the deployment server? [play] ")
     if service_name == "":
         service_name = "play"
 
@@ -225,13 +232,11 @@ def newSBS(app, args, env, cmdloader=None):
         cmdloader.commands['dependencies'].execute(command='dependencies', app=app, args=['--sync'], env=env, cmdloader=cmdloader)
 
     print "~ You created a new SBS Manager (but there are still some things to do):"
-    print "~   1. Create a repository in gitolite-admin"
-    print "~   2. Clone the repository"
-    print "~   3. Put this application in your local repository"
-    print "~   4. Put the SBS Manager Core as a submodule in repository/core (Core repository: git@github.com:digiPlant/sbsmanager.git)"
-    print "~   5. Set upp databases"
-    print "~   6. Add database user and passwords to conf/application.conf"
-    print "~   7. Start the app and go to http://localhost:9000/sbs/skrivbord/login/sysadmin and initialize the manager"
+    print "~   1. Create databases"
+    print "~   2. Add database user and passwords to conf/application.conf"
+    print "~   3. Create a repository in gitolite-admin"
+    print "~   4. Setup Git: from app dir run: play git-sbs"
+    print "~   5. Start the app and go to http://localhost:9000/sbs/skrivbord/login/sysadmin and initialize the manager"
     print "~"
     print "~ Server presumptions:"
     print "~   Play installed in: /opt/sbs-play"
@@ -243,6 +248,44 @@ def newSBS(app, args, env, cmdloader=None):
     print "~"
 
 process = None
+
+def handle_git_sigterm(signum, frame):
+    global process
+    if 'process' in globals():
+        process.terminate()
+        sys.exit(0)
+
+def gitSBS(app, args, env, cmdloader=None):
+    global process
+    print "~ Setting up git environment"
+    print "~ "
+    git_uri = raw_input("~ Git remote repository URI (git@store.digiplant.se:[customer]/sbsmanager.git)? ")
+    git_core_uri = raw_input("~ Git Core repository URI? [git@github.com:digiPlant/sbsmanager.git] ")
+    if git_core_uri == "":
+        git_core_uri = "git@github.com:digiPlant/sbsmanager.git"
+    git_commands = [
+        ["git", "init"], # Skapa git
+        ["git", "remote", "add", "--track", "master", "origin", git_uri], # Koppla mot remote
+        ["git", "submodule", "add", git_core_uri, "repository/core/"], # Koppla core submodule
+        ["git", "submodule", "update", "--init"]
+        #["cd", "repository/core"], 
+        #["git", "checkout", "`git describe --tags`"] # Checka ut den senaste taggade versionen av core
+    ]
+
+    for git_cmd in git_commands:   
+        try:
+            process = subprocess.Popen (git_cmd, env=os.environ)
+            signal.signal(signal.SIGTERM, handle_git_sigterm)
+            return_code = process.wait()
+            if 0 != return_code:
+                sys.exit(return_code)
+        except OSError, e:
+            print "Could not execute git command: %s " % git_cmd
+            print e
+            sys.exit(-1)
+        print
+
+    print "run git checkout `git describe --tags` manually from repository/core"
 
 def handle_sigterm(signum, frame):
     global process
